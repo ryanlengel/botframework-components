b40a70e76a42b4c90f5b3e2aeee158b011e7e1b8
Thomas Chung <thomas.chung@live.com>
2021-03-30 21:04:49 -0700

    Integrate RC0 startup stuff

--- skills/declarative/whoSkill/whoSkill/Controllers/BotController.cs
+++ skills/declarative/whoSkill/whoSkill/Controllers/BotController.cs
@@ -6,56 +6,71 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Runtime.Settings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
-namespace whoSkill.Controllers
+namespace whoSkill
 {
     // This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
     // implementation at runtime. Multiple different IBot implementations running at different endpoints can be
     // achieved by specifying a more specific type for the bot constructor argument.
     [ApiController]
-    public class BotController : ControllerBase
+public class BotController : ControllerBase
+{
+    private readonly Dictionary<string, IBotFrameworkHttpAdapter> _adapters = new Dictionary<string, IBotFrameworkHttpAdapter>();
+    private readonly IBot _bot;
+    private readonly ILogger<BotController> _logger;
+
+    public BotController(
+        IConfiguration configuration,
+        IEnumerable<IBotFrameworkHttpAdapter> adapters,
+        IBot bot,
+        ILogger<BotController> logger)
     {
-        private readonly Dictionary<string, IBotFrameworkHttpAdapter> _adapters = new Dictionary<string, IBotFrameworkHttpAdapter>();
-        private readonly IBot _bot;
+        _bot = bot ?? throw new ArgumentNullException(nameof(bot));
+        _logger = logger;
 
-        public BotController(
-            IEnumerable<IBotFrameworkHttpAdapter> adapters,
-            IEnumerable<AdapterSettings> adapterSettings,
-            IBot bot)
+        var adapterSettings = configuration.GetSection(AdapterSettings.AdapterSettingsKey).Get<List<AdapterSettings>>() ?? new List<AdapterSettings>();
+        adapterSettings.Add(AdapterSettings.CoreBotAdapterSettings);
+
+        foreach (var adapter in adapters ?? throw new ArgumentNullException(nameof(adapters)))
         {
-            _bot = bot ?? throw new ArgumentNullException(nameof(bot));
+            var settings = adapterSettings.FirstOrDefault(s => s.Enabled && s.Type == adapter.GetType().FullName);
 
-            foreach (var adapter in adapters ?? throw new ArgumentNullException(nameof(adapters)))
+            if (settings != null)
             {
-                var settings = adapterSettings.FirstOrDefault(s => s.Enabled && s.Name == adapter.GetType().FullName);
-
-                if (settings != null)
-                {
-                    _adapters.Add(settings.Route, adapter);
-                }
-            }
-        }
-
-        [HttpPost]
-        [HttpGet]
-        [Route("api/{route}")]
-        public async Task PostAsync(string route)
-        {
-            if (string.IsNullOrEmpty(route))
-            {
-                throw new ArgumentNullException(nameof(route));
-            }
-
-            if (_adapters.TryGetValue(route, out IBotFrameworkHttpAdapter adapter))
-            {
-                // Delegate the processing of the HTTP POST to the appropriate adapter.
-                // The adapter will invoke the bot.
-                await adapter.ProcessAsync(Request, Response, _bot).ConfigureAwait(false);
-            }
-            else
-            {
-                throw new KeyNotFoundException($"No adapter registered and enabled for route {route}.");
+                _adapters.Add(settings.Route, adapter);
             }
         }
     }
+
+    [HttpPost]
+    [HttpGet]
+    [Route("api/{route}")]
+    public async Task PostAsync(string route)
+    {
+        if (string.IsNullOrEmpty(route))
+        {
+            _logger.LogError($"PostAsync: No route provided.");
+            throw new ArgumentNullException(nameof(route));
+        }
+
+        if (_adapters.TryGetValue(route, out IBotFrameworkHttpAdapter adapter))
+        {
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogInformation($"PostAsync: routed '{route}' to {adapter.GetType().Name}");
+            }
+
+            // Delegate the processing of the HTTP POST to the appropriate adapter.
+            // The adapter will invoke the bot.
+            await adapter.ProcessAsync(Request, Response, _bot).ConfigureAwait(false);
+        }
+        else
+        {
+            _logger.LogError($"PostAsync: No adapter registered and enabled for route {route}.");
+            throw new KeyNotFoundException($"No adapter registered and enabled for route {route}.");
+        }
+    }
+}
 }
